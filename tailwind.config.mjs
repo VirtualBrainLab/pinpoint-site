e018cf88ba851c7bd424129a88e7594635e51bb2
kjy5 <82800265+kjy5@users.noreply.github.com>
2025-01-16 19:58:02 +0000

    Format code

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-import typography from '@tailwindcss/typography'
-import daisyui from 'daisyui'
+import typography from "@tailwindcss/typography";
+import daisyui from "daisyui";
 
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    typography,
-    daisyui,
-  ],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [typography, daisyui],
 
-  daisyui: {
-    themes: ['dark'],
-  },
-}
+	daisyui: {
+		themes: ["dark"],
+	},
+};
