dfd0d17af5729c3056aa415464118cbd28caadf2
Kenneth Yang <kjy5@uw.edu>
2025-01-16 16:00:40 -0800

    Theme and dan suggestions

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-import typography from "@tailwindcss/typography";
-import daisyui from "daisyui";
+import typography from '@tailwindcss/typography'
+import daisyui from 'daisyui'
+import { dark } from 'daisyui/src/theming/themes'
 
 export default {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [typography, daisyui],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [typography, daisyui],
 
-	daisyui: {
-		themes: ["dark"],
-	},
-};
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          ...dark,
+          'primary': '#0FBBBB',
+          'base-100': '#000000',
+          'base-content': '#ffffff',
+        },
+      },
+    ],
+  },
+}
