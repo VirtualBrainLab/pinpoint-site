72bfbe841bff3825f3b42211af3805670f0a5777
kjy5 <82800265+kjy5@users.noreply.github.com>
2025-01-18 07:00:57 +0000

    Format code

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,29 +1,29 @@
 /** @type {import('tailwindcss').Config} */
-import typography from '@tailwindcss/typography'
-import daisyui from 'daisyui'
-import { dark } from 'daisyui/src/theming/themes'
+import typography from "@tailwindcss/typography";
+import daisyui from "daisyui";
+import { dark } from "daisyui/src/theming/themes";
 
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {
-      height: {
-        '1/2dvh': '50dvh',
-      },
-    },
-  },
-  plugins: [typography, daisyui],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			height: {
+				"1/2dvh": "50dvh",
+			},
+		},
+	},
+	plugins: [typography, daisyui],
 
-  daisyui: {
-    themes: [
-      {
-        dark: {
-          ...dark,
-          primary: '#0FBBBB',
-          'base-100': '#000000',
-          'base-content': '#ffffff',
-        },
-      },
-    ],
-  },
-}
+	daisyui: {
+		themes: [
+			{
+				dark: {
+					...dark,
+					primary: "#0FBBBB",
+					"base-100": "#000000",
+					"base-content": "#ffffff",
+				},
+			},
+		],
+	},
+};
