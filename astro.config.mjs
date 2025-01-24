d779a901d131dbaa5451ae2325b75625cb4a9399
Kenneth Yang <kjy5@uw.edu>
2025-01-24 00:17:06 -0800

    Remove smoothing, set size to image, change domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://virtualbrainlab.github.io",
-	base: "pinpoint-site",
+	site: 'https://pinpoint.virtualbrainlab.org',
 	integrations: [tailwind()],
 });
