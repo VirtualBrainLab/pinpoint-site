d3ff377730d2a4e2230ad837f699a87a9f2f0c20
kjy5 <82800265+kjy5@users.noreply.github.com>
2025-02-06 08:43:01 +0000

    Format code

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-import typography from '@tailwindcss/typography'
-import daisyui from 'daisyui'
-import { dark } from 'daisyui/src/theming/themes'
+import typography from "@tailwindcss/typography";
+import daisyui from "daisyui";
+import { dark } from "daisyui/src/theming/themes";
 
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
 			maxHeight: {
-				'1/2dvh': '50dvh',
+				"1/2dvh": "50dvh",
 			},
 			height: {
 				"1/2dvh": "50dvh",
